b5eb1f24aca57ccb07c24955e213798d110dbdb9
qaziahmedali <hqazi1731@gmail.com>
2025-08-29 21:57:25 +0500

    Update Import Paths in MainLayout for Consistent Component Structure
    - Refactored import statements in MainLayout.tsx to align with the new directory structure, moving Navbar and Footer imports to the 'components/ui' directory.
    - Improved code organization and maintainability by ensuring consistent import paths across the application.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../ui/Navbar";
-import Footer from "../ui/Footer";
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/ui/Navbar';
+import Footer from '../components/ui/Footer';
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-background text-foreground overflow-x-hidden">
+    <div className='min-h-screen flex flex-col bg-background text-foreground overflow-x-hidden'>
       <Navbar />
-      <main className="flex-grow">
+      <main className='flex-grow'>
         <Outlet />
       </main>
       <Footer />
